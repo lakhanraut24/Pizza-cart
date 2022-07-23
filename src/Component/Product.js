7cb7eee9c3a55e1a6e40771e55b216a71fa99545
lakhan <lakhanraut24@gmail.com>
2022-07-23 21:21:31 +0530

    Add

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -21,7 +21,7 @@ const Product = (props) => {
           <div className="flex  justify-between items-center mt-4">
             <span>₹ {dataval.price}</span>
             <button className="bg-yellow-500 py-1 px-4 rounded-full font-bold">
-              ADD
+              Add
             </button>
           </div>
         </div>
